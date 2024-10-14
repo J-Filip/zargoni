d3fe2e04576134f1ad292f622228a271da3c5ba6
J-Filip <filip.jugkala@yahoo.com>
2024-10-14 18:49:56 +0200

    setup typescript, add path alias for cleaner imports

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+});
